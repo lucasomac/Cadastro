83e4df14e21c632b852331b196b80ade99d0aa76
jeehmilena <jessicamilenacorrea@gmail.com>
2020-09-14 22:24:13 -0300

    modificações da jess

--- src/Aula.kt
+++ src/Aula.kt
@@ -1,4 +1,3 @@
 import java.sql.Time
 
-class Aula(var materia: Materia, var horaInicio: Time, var horaTermino: Time) {
-}
+class Aula(var materia: Materia, var horaInicio: Time, var horaTermino: Time)
--- src/Professor.kt
+++ src/Professor.kt
@@ -1,4 +1,4 @@
-class Professor (val rd: Int, var nome: String) : Docencia{
+class Professor(val rd: Int, var nome: String) : Docencia {
     override fun darAulas(vararg aulas: Aula) {
         aulas.forEach {
             println("Dando aula da matéria ${it.materia.nome}, que começa as ${it.horaInicio}" +
@@ -6,10 +6,8 @@ class Professor (val rd: Int, var nome: String) : Docencia{
         }
     }
 
-    override fun fazerChamada(vararg alunos : Aluno) {
+    override fun fazerChamada(vararg alunos: Aluno) {
         println("Fazendo chamada")
         alunos.forEach { println("Chamando ${it.nome} ${it.sobreNome}") }
     }
-
-
 }
